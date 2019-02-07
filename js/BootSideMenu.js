434f3df2a2844f0aa246d22adeb64783cab8d0e2
Brian Geyer <geyerbri@msu.edu>
2019-02-06 21:51:04 -0500

    arh

--- js/BootSideMenu.js
+++ js/BootSideMenu.js
@@ -235,7 +235,7 @@
             } else if (options.side === "right") {
                 $menu.status = "closed";
                 $menu.hide().animate({
-                    right: -($menu.width() + 35)
+                    right: -($menu.width() + 33)
                 }, 1, function () {
                     $menu.show();
                     switchArrow("right");
@@ -294,7 +294,7 @@
                 }
 
                 $menu.animate({
-                    right: -($menu.width() + 35)
+                    right: -($menu.width() + 33)
                 }, {
                     duration: options.duration,
                     done: function () {
